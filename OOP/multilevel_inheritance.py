32cae1b8d7e3dbba603730135bfa9cbf97d15c74
shuvro-baset <md.abdul.baset75@gmail.com>
2023-04-19 02:31:02 +0600

    chore(class): types of inheritance and overriding

--- OOP/multilevel_inheritance.py
+++ OOP/multilevel_inheritance.py
@@ -17,3 +17,32 @@ ch1 = ChildClass2()
 ch1.method1()
 ch1.method2()
 ch1.method3()
+
+# ===============================
+class Student:
+    def __init__(self, name, Id):
+        self.name = name 
+        self.Id = Id
+        
+    def details(self):
+        print("Name: ", self.name, "Id: ", self.Id)
+
+class CSEStudent(Student):
+    def __init__(self, name, Id, labs):
+        super().__init__(name, Id)
+        self.no_of_labs = labs
+        
+    def cry(self):
+        print("CSE Student is crying because of", self.no_of_labs, "labs")
+        
+        
+class CSEFresher(CSEStudent):
+    def enroll_CSE110(self):
+        print(self.name, "enrolled in CSE110")
+        
+#====== 
+s1 = CSEFresher("Bob", 11, 4)
+s2 = CSEFresher("Carol", 12, 4)
+s2.details()
+s1.details()
+s2.enroll_CSE110()
